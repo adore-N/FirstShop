cd552b7f9f721d6517667c32d2271d412166fb9e
adore-N <819790662@qq.com>
2019-09-08 13:26:41 +0800

    创建PHP接口,主要是商品列表的数据获取

---
+++ data/goodslist.php
@@ -0,0 +1,33 @@
+<?php
+    //   //接受数据
+    $goods_type = @$_REQUEST["goodsType"];
+    // $user_phone = "1";
+    // $goods_type = "秒杀";
+    // $user_pwd = @$_REQUEST["userPwd"];
+    
+    //连接数据库
+    $link = @new mysqli("localhost","root","root","datas");
+    echo $link->connect_error;
+
+    // $sql = "SELECT * FROM three WHERE user_phone='".$user_phone."' AND user_pwd='".$user_pwd."'";
+    $sql = "SELECT Id,goods_price,goods_intro,goods_img FROM goodsinfo WHERE goods_type LIKE '%".$goods_type."%'";
+
+    // echo $sql;
+    $q = $link->query($sql);
+    // $qq = $link->query($sql1);
+    // echo $sql1;
+    // echo $q;
+    if($q->num_rows > 0){
+        $str = "";
+        while($arr = $q->fetch_assoc()){
+            $str = $str.json_encode($arr).",";
+        }
+        $str = "[".substr($str,0,-1)."]";
+        echo '{"code":1,"msg":'.$str.'}';
+    }else{
+        echo '{"code":0,"msg":"没有数据"}';
+    }
+
+
+
+?>
